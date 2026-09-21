4b057b810265a095605e16ed99007ba112497396
abz344467-del <abz344467@gmail.com>
2026-09-21 20:40:20 +0100

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+#include "main.h"
+
+void ScriptMain()
+{
+    // ABZZMODZ will be initialized here.
+    // The menu implementation comes next.
+    while (true)
+    {
+        WAIT(0);
+    }
+}
